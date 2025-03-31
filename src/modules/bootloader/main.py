d37c1c89477526b0ece026413b2b9fbb6533e7db
Vasiliy Stelmachenok <ventureo@cachyos.org>
2025-03-31 20:24:33 +0300

    bootloader: Fix autoboot setup
    Signed-off-by: Vasiliy Stelmachenok <ventureo@cachyos.org>

--- src/modules/bootloader/main.py
+++ src/modules/bootloader/main.py
@@ -741,6 +741,7 @@ def update_limine_config(efi_directory, installation_root_path):
 
     with open(config_path, 'w') as config_file:
         config_file.write("timeout: 5\n")
+        config_file.write("default_entry: 2\n")
 
         # Copy splash logo
         try:
@@ -762,7 +763,7 @@ def update_limine_config(efi_directory, installation_root_path):
             with open(kernel_pkgbase) as kernel_pkgbase_file:
                 kernel_name = kernel_pkgbase_file.readline().strip()
 
-            config_file.write(f"/CachyOS\n")
+            config_file.write(f"/+CachyOS\n")
             config_file.write(f"//{kernel_name} kernel\n")
             config_file.write(f"\tprotocol: linux\n")
             config_file.write(f"\tkernel_path: boot():/vmlinuz-{kernel_name}\n")

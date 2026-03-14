0121f2439a1a894cfd3814dba6ba05b127b4f48c
Peter Jung <admin@ptr1337.dev>
2026-03-14 17:13:43 +0100

    plymouthcfg: Add support for AMD GPU-specific Plymouth theme
    Some Plymouth themes are broken on specific AMD GPUs. Add a
    plymouth_theme_amdgpu config variable that, when set, overrides the
    default theme on systems with an AMD GPU. Detection is done via sysfs
    PCI device enumeration (vendor 0x1002 + display device class)
    
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- src/modules/plymouthcfg/main.py
+++ src/modules/plymouthcfg/main.py
@@ -12,6 +12,8 @@
 #   Calamares is Free Software: see the License-Identifier above.
 #
 
+import os
+
 import libcalamares
 
 from libcalamares.utils import debug, target_env_call
@@ -21,6 +23,11 @@ _ = gettext.translation("calamares-python",
                         localedir=libcalamares.utils.gettext_path(),
                         languages=libcalamares.utils.gettext_languages(),
                         fallback=True).gettext
+
+# AMD PCI vendor ID
+AMD_VENDOR_ID = "0x1002"
+# PCI display device classes (VGA controller and 3D controller)
+DISPLAY_CLASSES = ("0x030000", "0x030200", "0x038000")
 
 
 def pretty_name():
@@ -37,6 +44,33 @@ def detect_plymouth():
     return target_env_call(["sh", "-c", "which plymouth"]) == 0
 
 
+def detect_amdgpu():
+    """
+    Checks if an AMD GPU is present by reading PCI device info from sysfs.
+
+    @return True if an AMD GPU is detected, False otherwise
+    """
+    pci_devices = "/sys/bus/pci/devices"
+    try:
+        for device in os.listdir(pci_devices):
+            device_path = os.path.join(pci_devices, device)
+            vendor_path = os.path.join(device_path, "vendor")
+            class_path = os.path.join(device_path, "class")
+            try:
+                with open(vendor_path, "r") as f:
+                    vendor = f.read().strip()
+                with open(class_path, "r") as f:
+                    dev_class = f.read().strip()
+            except OSError:
+                continue
+            if vendor == AMD_VENDOR_ID and dev_class in DISPLAY_CLASSES:
+                debug("Detected AMD GPU: {}".format(device))
+                return True
+    except OSError:
+        debug("Could not read PCI devices from sysfs")
+    return False
+
+
 class PlymouthController:
 
     def __init__(self):
@@ -47,8 +81,14 @@ class PlymouthController:
         return self.__root
 
     def setTheme(self):
-        plymouth_theme = libcalamares.job.configuration["plymouth_theme"]
-        target_env_call(["plymouth-set-default-theme",  plymouth_theme])
+        config = libcalamares.job.configuration
+        plymouth_theme = config["plymouth_theme"]
+
+        if config.get("plymouth_theme_amdgpu") and detect_amdgpu():
+            plymouth_theme = config["plymouth_theme_amdgpu"]
+            debug("Using AMD GPU plymouth theme: {}".format(plymouth_theme))
+
+        target_env_call(["plymouth-set-default-theme", plymouth_theme])
 
     def run(self):
         if detect_plymouth():
